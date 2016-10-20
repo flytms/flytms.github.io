8f9c191d236ee0f4cec65dfaccba5462f9d3cc87
flytms <flytms@tutanota.com>
2016-10-20 21:15:20 +0300

    cv

--- cvcompany/js/common.js
+++ cvcompany/js/common.js
@@ -105,5 +105,23 @@ $(document).ready(function() {
 		});
 		return false;
 	});
+    
+    //get URL parametrs
+    var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+    };
+
+    $("span.company").text(getUrlParameter('company'));
+    $("span.position").text(getUrlParameter('position'));
 });
